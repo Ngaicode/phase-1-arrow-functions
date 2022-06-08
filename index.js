94a85e932303315de0e4da8bdf01a5afc3340a08
Ngaicode <mjombakevin376@gmail.com>
2022-06-08 22:10:30 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let divide = function () {
+  return 2000 / 100;
+};
+let square = (value) => {
+  return value * value;
+};
+let add = (first, second) => {
+  return first + second;
+};
